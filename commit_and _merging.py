a61eff05624898311c4d02b16935e94c8895b440
Annkimani <annkimani270@gmail.com>
2018-08-09 22:33:05 +0300

    commit_and _merging.py

--- commit_and _merging.py
+++ commit_and _merging.py
@@ -1,10 +1,12 @@
 def multiplication(x,y):
     pass
 def addition(x,y):
-    pass
+    return (x + y)
+    return results
 def division (x,y):
     return (x / y)
     return results
 def subtraction(x,y):
     pass
 print(15 / 3)
+print(5 + 9)
